695c6009745ba026e6fd8db386ad61168bff877b
yzm <4463215@qq.com>
2022-11-25 17:21:37 +0800

    feat: v1.0.1，新增tabbar，设置页

--- App.vue
+++ App.vue
@@ -12,6 +12,26 @@
 	}
 </script>
 
-<style>
-	/*每个页面公共css */
+<style lang="scss">
+	// .uni-app--showtabbar uni-page-head[uni-page-head-type="default"]~uni-page-wrapper {
+	// 	height: calc(100% - 44px - env(safe-area-inset-top)) !important;
+	// }
+
+	// // .uni-app--showtabbar uni-page-wrapper:after {
+	// //     height: 80rpx;
+	// //     height: calc(80rpx + constant(safe-area-inset-bottom));
+	// //     height: calc(80rpx + env(safe-area-inset-bottom));
+	// // }
+	// uni-tabbar {
+	// 	.uni-tabbar {
+	// 		// tab 背景
+	// 		border-radius: 10px;
+	// 		margin: 20px;
+	// 		box-shadow: 0 0 10px #00000045;
+	// 		.uni-tabbar-border {
+	// 			// tabBar 上边框
+	// 			background-color: transparent !important; // tabBar 上边框的颜色
+	// 		}
+	// 	}
+	// }
 </style>
--- pages/store/index.vue
+++ pages/store/index.vue
@@ -52,7 +52,7 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.content {
 		display: flex;
 		flex-direction: column;
@@ -77,5 +77,32 @@
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
+	}
+	
+	.uni-app--showtabbar uni-page-head[uni-page-head-type="default"]~uni-page-wrapper {
+		height: calc(100% - 44px - env(safe-area-inset-top)) !important;
+	}
+	
+	// .uni-app--showtabbar uni-page-wrapper:after {
+	//     height: 80rpx;
+	//     height: calc(80rpx + constant(safe-area-inset-bottom));
+	//     height: calc(80rpx + env(safe-area-inset-bottom));
+	// }
+	
+	uni-tabbar {
+		.uni-tabbar {
+			// tab 背景
+			border-radius: 10px;
+			margin: 20px;
+			box-shadow: 0 0 10px #00000045;
+			.uni-tabbar-border {
+				// tabBar 上边框
+				background-color: transparent !important; // tabBar 上边框的颜色
+			}
+		}
+
+		.uni-tabbar~.uni-placeholder {
+			height: 80rpx;
+		}
 	}
 </style>
